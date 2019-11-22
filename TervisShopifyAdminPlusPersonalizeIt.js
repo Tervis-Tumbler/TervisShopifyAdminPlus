2f96727eab1a568004b653832e5b60015c2ef0fe
ChrisMagnuson <hourglasssand@gmail.com>
2019-11-21 20:10:55 -0500

    2nd try to remove empty elements

--- TervisShopifyAdminPlusPersonalizeIt.js
+++ TervisShopifyAdminPlusPersonalizeIt.js
@@ -676,16 +676,18 @@ function Get_TervisPersonalizationNumberSides ({
 async function Get_TervisPersonalizationFormProperties () {
     var $Properties = {}
     document.querySelectorAll("#PersonalizationInformationContainer input:not([id*='Monogram']):not([hidden])")
+    .filter( $Node => $Node.value)
     .forEach(
         $Node => $Properties[$Node.id] = $Node.value
     )
 
     document.querySelectorAll("#PersonalizationInformationContainer input[id*='Monogram']:not([hidden])")
+    .filter( $Node => $Node.value)
     .forEach(
         $Node => $Properties[$Node.id.replace(/MonogramAllCharactersRequired/, "").replace(/MonogramAllCharactersNotRequired/, "")] = $Node.value
     )
 
-    return Remove_ObjectKeyWithEmptyOrNullValue($Properties)
+    return $Properties
 }
 
 function Get_TervisShopifyPOSPersonalizableLineItemAssociatedPersonalizationChargeLine ({
