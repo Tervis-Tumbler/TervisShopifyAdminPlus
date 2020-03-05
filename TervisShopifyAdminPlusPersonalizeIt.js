343297ddca3ccbd5c5684e885868042512153e57
Hamilton Perez <hambone124@users.noreply.github.com>
2020-03-04 23:00:20 -0500

    Try hard coded values

--- TervisShopifyAdminPlusPersonalizeIt.js
+++ TervisShopifyAdminPlusPersonalizeIt.js
@@ -775,8 +775,8 @@ async function Receive_TervisShopifyPOSPersonalizationSaveOnClick () {
         Shopify.fetchCart({
             success: cart => {
                 cart.addLineItem({
-                    variant_id: Number.parseInt($PersonalizationFeeSKU),
-                    quantity: $Quantity
+                    variant_id: 1154266,
+                    quantity: 1
                 },{
                     success: ShopifyPOS.flashNotice("Added PERS FEE"),
                     error: errors => ShopifyPOS.flashError(errors)
